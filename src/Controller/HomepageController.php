4febc1920b9cdcd73ea91608d36eaef7da3b60b7
Petr Simecek <psimecek87@gmail.com>
2024-06-26 23:29:18 +0200

    chore: add logger + catch request errors

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,6 +2,8 @@
 namespace App\Controller;
 
 use App\Module\Cacher\Jobs as CacherJobs;
+use Psr\Log\LoggerInterface;
+use RecruitisApi\ApiException;
 use RecruitisApi\Configuration;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,21 +12,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(LoggerInterface $logger): Response
     {
         $conf = (new Configuration())->setAccessToken($this->getParameter('recruitis.api.token'));
         $cache = new CacherJobs($conf);
-        $jobs = $cache->getJobs();
+        try {
+            $jobs = $cache->getJobs();
+        } catch (ApiException $exception){
+            return $this->showError($exception, $logger);
+        }
         return $this->render('homepage/index.html.twig', ['jobs' => $jobs]);
     }
 
     #[Route('/job/{id}', name: "job-detail")]
-    public function job($id): Response
+    public function job(LoggerInterface $logger, $id): Response
     {
         $conf = (new Configuration())->setAccessToken($this->getParameter('recruitis.api.token'));
 
         $cache = new CacherJobs($conf);
-        $job = $cache->getJob($id);
+        try {
+            $job = $cache->getJob($id);
+        } catch (ApiException $exception){
+            return $this->showError($exception, $logger);
+        }
         return $this->render('homepage/job.html.twig', ['job' => $job]);
+    }
+
+    private function showError(ApiException $exception, LoggerInterface $logger): Response {
+        $logger->error($exception);
+        return $this->render('error.html.twig', ['exception' => $exception]);
     }
 }
